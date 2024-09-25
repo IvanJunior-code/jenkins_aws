d82f9518f81b69c909fe091c920f495c1bdaac38
Ivanzinho <ivan_junior_we@hotmail.com>
2024-09-24 23:08:41 -0300

    Alterando a forma de instalação do kubectl no servidor Jenkins

--- IaC/main.tf
+++ IaC/main.tf
@@ -48,10 +48,10 @@ resource "aws_instance" "jenkins_server" {
                  sudo usermod -aG docker jenkins
  
                  # Instalação do kubectl
-                 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
-                 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-                 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-                 sudo apt-get update && sudo apt-get install -y kubectl
+                 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+                 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+                 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+                 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
                  # Reiniciando o Jenkins
                  sudo systemctl restart jenkins
